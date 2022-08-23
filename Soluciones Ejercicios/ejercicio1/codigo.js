ddd84886206711c5e72c05e0ada94c79dbf53fc3
“BreakingNS” <nahuel_segura_17@hotmail.com>
2022-08-23 13:47:20 -0300

    first commit

---
+++ Soluciones Ejercicios/ejercicio1/codigo.js
@@ -0,0 +1,6 @@
+let cadena = "cadena de prueba";
+let cadena2 = "cadena ";
+
+resultado = cadena.startsWith(cadena2);
+
+document.write(resultado);
---
+++ nuevo/codigo.js
@@ -0,0 +1,5 @@
+monto = prompt("¿Cuanto dinero tienes?");
+
+dineroCofla = prompt("¿Cuanto dinero tiene Cofla?");
+dineroNahuel = prompt("¿Cuanto dinero tiene Nahuel?");
+dineroTapia = prompt("¿Cuanto dinero tiene Tapia?");
